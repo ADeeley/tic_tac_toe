3876b32e1284063a9bfd0bd7f685a5a62064bce1
Adam Deeley <ad.deeley@gmail.com>
2017-11-05 12:16:28 +0000

    Added a main game loop function and click events for the table that populate test.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+var player = {
+    counter : undefined
+}
+
 function counterChoice() {
     /**
      * Displays two buttons, giving the player a choice of X's or O's.
@@ -18,24 +22,48 @@ function counterChoice() {
     $("#content").append(box);
 }
 
+function gameComplete() {
+    /**
+     * Iterates over the board and checks for any completed lines.
+     */
+    $("#gameBoard tr").each(function() {
+        $(this).find("td").each(function() {
+            $(this).html("test");
+        })
+    })
+}
+
 function createBoard() {
     /**
      * Displays the game board and waits for the player to place a piece.
      */
+    $("#content").empty();
     var board = $("<table><tr><td></td><td></td><td></td><tr><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td></tr></table>")
-        .addClass("board");
+        .addClass("board")
+        .attr("id", "gameBoard");
     $("#content").append(board);
 }
+
 window.onload = function() {
+    // First screen - counter choice
+
     counterChoice();
-    $("button").click(function() {
-        if ($(this).text() == "X") {
-            $("#content").empty();
-            createBoard();
-        }
-        else {
-            $("#content").empty();
-            createBoard();
-        }
+    $("button").one("click", function() {
+        player.counter = $(this).html();
+        console.log("Player has chosen : " + player.counter);
+        createBoard();
+        mainGameHandler();
     })
+}
+
+function mainGameHandler() {
+    /**
+     * Control is passed to this function when the player has chosen a counter type.
+     */
+    console.log("MainGame Handler");
+    $("#gameBoard td").on("click", function() {
+        console.log("Recorded click");
+        $(this).html("Test");
+    })
+
 }
