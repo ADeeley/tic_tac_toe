040784bb4c2d0c1be8c43c2fceb8f5998826880b
Adam Deeley <ad.deeley@gmail.com>
2017-11-05 14:36:06 +0000

    Added checkForEndgame and endgameSequence functions.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,10 @@ var cpu = {
     counter : undefined
 }
 
+var winningCombinations = [["1","2","3"], ["4","5","6"], ["7","8","9"],
+                           ["1","4","7"], ["2","5","8"], ["3","6","9"],
+                           ["1","5","9"], ["7","5","3"]]; 
+
 function counterChoice() {
     /**
      * Displays two buttons, giving the player a choice of X's or O's.
@@ -42,10 +46,11 @@ function createBoard() {
      * Displays the game board and waits for the player to place a piece.
      */
     $("#content").empty();
-    var board = $("<table><tr><td></td><td></td><td></td><tr><td></td><td></td><td></td></tr><tr><td></td><td></td><td></td></tr></table>")
+    var board = $("<table><tr><td id='1'></td><td id='2'></td><td id='3'></td><tr><td id='4'></td><td id='5'></td><td id='6'></td></tr><tr><td id='7'></td><td id='8'></td><td id='9'></td></tr></table>")
         .addClass("board")
         .attr("id", "gameBoard");
     $("#content").append(board);
+
 }
 
 function placeCPUCounter() {
@@ -73,6 +78,60 @@ function placeCPUCounter() {
     chosenCell.html(cpu.counter); 
 }
 
+function checkForEndgame() {
+    /**
+     * Checks the cells for matching lines.
+     */
+    // (1) generate array of counter positions for x and for o
+    // (2) for x and o, separately, check if any of the winning combinations are within
+    // the arrays
+    var xCells = [];
+    var oCells = [];
+
+    $("#gameBoard tr").each(function() {
+        $(this).find("td").each(function() {
+            let candidate = $(this);
+            if (candidate.html() == "X") {
+                xCells.push(candidate.attr("id"));
+            }
+            else if (candidate.html() == "O") {
+                oCells.push(candidate.attr("id"));
+            }
+        })
+    })
+    //console.log("Xcells: " + xCells + " oCells: " + oCells);
+
+    // Check for X
+    for (var i=0; i<winningCombinations.length; i++) {
+        var matches = 0;
+        for (var j=0; j<winningCombinations[i].length; j++) {
+            if (xCells.indexOf(winningCombinations[i][j]) != -1) {
+                matches++;
+            }
+        }
+        if (matches == 3) {
+            console.log("X Wins");
+            return "X";
+        }
+    }
+    
+    // Check for O
+    for (var i=0; i<winningCombinations.length; i++) {
+        var matches = 0;
+        for (var j=0; j<winningCombinations[i].length; j++) {
+            if (oCells.indexOf(winningCombinations[i][j]) != -1) {
+                matches++;
+            }
+        }
+        if (matches == 3) {
+            console.log("O Wins");
+            return "O";
+        }
+    }
+    // no winners
+    return -1;
+}
+
 window.onload = function() {
     // First screen - counter choice
 
@@ -93,6 +152,10 @@ window.onload = function() {
     })
 }
 
+function endGameSequence(victor) {
+    console.log("Victory for " + victor);
+}
+
 function mainGameHandler() {
     /**
      * Control is passed to this function when the player has chosen a counter type.
@@ -102,9 +165,19 @@ function mainGameHandler() {
         console.log("Recorded click");
         if ($(this).html() == "") {
             console.log("Valid click");
+
             $(this).html(player.counter);
+            var victor = checkForEndgame();
+            if (victor != -1) {
+                return endGameSequence(victor);
+            }
+            // CPU turn
             placeCPUCounter();
-            // check victory
+            checkForEndgame();
+            var victor = checkForEndgame();
+            if (victor != -1) {
+                return endGameSequence(victor);
+            }
         }
         
     })
