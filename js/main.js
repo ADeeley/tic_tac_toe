8d3b9eb16e6704c395882113d538dc74f0135e9f
Adam Deeley <ad.deeley@gmail.com>
2017-11-11 18:07:31 +0000

    Added new branch to avoid using JQuery.

--- js/main.js
+++ js/main.js
@@ -7,74 +7,59 @@ Set.prototype.isSuperset = function(subset) {
     return true;
 }
 
-var player = {
-    counter : undefined
+var states  = {
+    counterChoice : null,
+    gameBoard : null,
+    endGame : null
+
 }
 
-var cpu = {
-    counter : undefined
-}
-
-var winningCombos = [["1","2","3"], ["4","5","6"], ["7","8","9"],
-                           ["1","4","7"], ["2","5","8"], ["3","6","9"],
-                           ["1","5","9"], ["7","5","3"]]; 
-
-
-function counterChoice() {
-    /**
-     * Displays two buttons, giving the player a choice of X's or O's.
-     */
-    var instructText = $("<h2 id='instructText'>Choose your marker</h2>")
-    $("#content").empty();
-    var orTxt = $("<h3 id='counterOr'>or</h3>");
-    var xBtn = $("<button id='xButton' class='counterChoiceButton'>X</button")
-        .text("X")
-    var oBtn = $("<button id='oButton' class='counterChoiceButton'>O</button")
-        .text("O");
-    var box = $("<div class='counterChoice'></div>")
-        .append(xBtn, orTxt, oBtn);
-
-    $("#content").append(instructText, box);
-}
-
-function gameComplete() {
-    /**
-     * Iterates over the board and checks for any completed lines.
-     */
-    $("#gameBoard tr").each(function() {
-        $(this).find("td").each(function() {
-            $(this).html("test");
-        })
-    })
-}
-
-function createBoard() {
-    /**
-     * Displays the game board and waits for the player to place a piece.
-     */
-    /*
-    $("#content").empty();
-    var board = $("<table><tr><td id='1'></td><td id='2'></td><td id='3'></td><tr><td id='4'></td><td id='5'></td><td id='6'></td></tr><tr><td id='7'></td><td id='8'></td><td id='9'></td></tr></table>")
-        .addClass("board")
-        .attr("id", "gameBoard");
-    $("#content").append(board);
-    */
-    var content = $("#content");
-    var board = $("<table></table")
-        .addClass("board")
-        .attr("id", "gameBoard");
-    var id = 1;
-
-    for (r=0; r<3; r++) {
-        var row = $("<tr></tr>");
-        for (c=0; c<3; c++) {
-            row.append("<td id=" + id++ + "></td>");
-        }
-        board.append(row);
+states.changeTo = function(changeTo) {
+    switch (changeTo) {
+        case "counterChoice":
+            states.counterChoice.style.display = "block";
+            states.gameBoard.style.display = "none";
+            states.endGame.style.display = "none";
+            break;
+        case "gameBoard":
+            states.counterChoice.style.display = "none";
+            states.gameBoard.style.display = "block";
+            states.endGame.style.display = "none";
+            break;
+        case "endGame":
+            counterChoice.style.display = "none";
+            gameBoard.style.display = "block";
+            endGame.style.display = "block";
+            break;
     }
-    content.empty();
-    content.append(board);
+} 
+
+var players = {
+    user : undefined,
+    cpu : undefined
 }
+
+players.chooseCounter = function(choice) {
+    if (choice === "X") {
+        players.user = "X";
+        players.cpu = "O";
+    }
+    else {
+        players.user = "O";
+        players.cpu = "X";
+    }
+    console.log("Player has chosen : " + players.user);
+    console.log("CPU is: " + players.cpu);
+}
+
+var winningCombos = [["1","2","3"], 
+                     ["4","5","6"], 
+                     ["7","8","9"],
+                     ["1","4","7"], 
+                     ["2","5","8"], 
+                     ["3","6","9"],
+                     ["1","5","9"], 
+                     ["7","5","3"]]; 
 
 function placeCPUCounter() {
     /**
@@ -141,62 +126,48 @@ function highlightWinningLine(arr) {
     }
 }
 
-function declareWinner(winner) {
-    var alertBacking = $("<div></div>")
-        .attr("id", "alertBacking");
-    var alertText = $("<h2>" + winner + " wins</h2>");
-    alertBacking.append(alertText);
-    $("#gameBoard").append(alertBacking);
+function click() {
+    console.log(event.target.innerHTML);
 }
 
 window.onload = function() {
-    // First screen - counter choice
-
-    counterChoice();
-    $("button").one("click", function() {
-        player.counter = $(this).html();
-        if ($(this).html() == "X") {
-            cpu.counter = "O";
-        }
-        else {
-            cpu.counter = "X";
-        }
-
-        console.log("Player has chosen : " + player.counter);
-        console.log("CPU is: " + cpu.counter);
-        createBoard();
-        mainGameHandler();
-    })
-}
-
-function endGameSequence(victor) {
-    console.log("Victory for " + victor);
-}
-
-function mainGameHandler() {
     /**
-     * Control is passed to this function when the player has chosen a counter type.
+     * Sets the initial variables and passes control to the main game handler.
      */
-    var gameCompleted = false;
-    console.log("MainGame Handler");
+    // Set the states to elements of the DOM
+    states.counterChoice = document.getElementById("counterChoice");
+    states.gameBoard = document.getElementById("gameBoard");
+    states.endGame = document.getElementById("endGameALert");
 
-    $("#gameBoard td").on("click", function() {
-        
-        if ($(this).html() == "" && gameCompleted == false) {
-            $(this).html(player.counter);
-            var victor = checkForEndgame();
-            if (victor) {
-                gameCompleted = true;
-                return endGameSequence(victor);
-            }
-            // CPU turn
-            setTimeout(placeCPUCounter, Math.random()* 1000);
-            checkForEndgame();
-            var victor = checkForEndgame();
-            if (victor) {
-                gameCompleted = true;
-                return endGameSequence(victor);
-            }
-        }
-    })
+    var buttons = document.getElementById("buttons"); 
+    buttons.addEventListener("click", eventControler.chooseCounter);
 }
+
+var eventControler = { 
+    /**
+     * Controls the flow of the game
+     */
+    chooseCounter : function() {
+        players.chooseCounter(event.target.innerHTML);
+        eventControler.playGame();
+    },
+
+    playGame : function() {
+        console.log("Reached playgame");
+        //add promise for user placing counter
+        //user places counter
+        //if user victory:
+        //      endgame
+        //else:
+        //      cpu places counter
+        //      if cpu victory:
+        //              endgame
+        //      else:
+        //              add new player promise
+        //      
+    }
+
+}
+
+
+
